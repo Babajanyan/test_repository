78af8b454556299ab5570fbb10bf404324354794
Babajanyan <johan@mail.ru>
2017-03-02 16:39:06 +0400

    first commmit

---
+++ lesson8.py
@@ -0,0 +1,34 @@
+def divider():
+    while True:
+        nums=input('enter 2 digits(format: x y ):')
+        (x,y)=nums.split()
+        try:
+            x = float(x)
+            y = float(y)
+            print(x/y)
+        except ZeroDivisionError:
+            print('Try again without zero')
+        except ValueError:
+            print('Try again without letters')
+        except:
+            print('something wrong')
+            raise
+
+def read_data(filename):
+    lines = []
+    infile = None
+    try:
+        infile = open(filename, encoding="utf8")
+        for line in infile:
+                lines.append(line)
+    except (IOError, OSError) as err:
+        print(err)
+        return []
+    finally:
+        if infile is not None:
+            infile.close()
+    return lines
+
+read_data('age.txt')
+#TODO lav chi
+#print(lines)
